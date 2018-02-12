cfade88a2d3cc5c400b4208f0a8e9d073b4722bf
Liam O'Hanlon <liam.ohanlon@answerdigital.com>
2018-02-12 09:20:17 +0000

    Adds tests for unit controller.

--- src/main/java/answer/king/controller/ItemController.java
+++ src/main/java/answer/king/controller/ItemController.java
@@ -1,30 +1,39 @@
 package answer.king.controller;
 
-import java.util.List;
-
+import answer.king.model.Item;
+import answer.king.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import answer.king.model.Item;
-import answer.king.service.ItemService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/item")
 public class ItemController {
 
-	@Autowired
-	private ItemService itemService;
+    private final ItemService itemService;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Item> getAll() {
-		return itemService.getAll();
-	}
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
 
-	@RequestMapping(method = RequestMethod.POST)
-	public Item create(@RequestBody Item item) {
-		return itemService.save(item);
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Item>> getAll() {
+        final List<Item> items = itemService.getAll();
+        if (items == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(items);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Item create(@RequestBody Item item) {
+        return itemService.save(item);
+    }
 }
