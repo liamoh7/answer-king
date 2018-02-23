6d29a074647ec5fa4257495b3103bb6ab5931c31
Liam O'Hanlon <liam.ohanlon@answerdigital.com>
2018-02-23 09:04:11 +0000

    Fixes password encoding.
    Fixes password encoding.

--- src/main/java/answer/king/security/AuthConfig.java
+++ src/main/java/answer/king/security/AuthConfig.java
@@ -5,18 +5,23 @@ import answer.king.repo.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AuthConfig extends GlobalAuthenticationConfigurerAdapter {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void init(AuthenticationManagerBuilder auth) {
@@ -25,6 +30,14 @@ public class AuthConfig extends GlobalAuthenticationConfigurerAdapter {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
     }
 
     @Bean
--- src/main/java/answer/king/security/SecurityConfig.java
+++ src/main/java/answer/king/security/SecurityConfig.java
@@ -1,11 +1,17 @@
 package answer.king.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DaoAuthenticationProvider authenticationProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -13,8 +19,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/register", "/", "/swagger-ui.html").permitAll()
                 .anyRequest().fullyAuthenticated()
                 .and()
+                .logout().logoutUrl("/logout")
+                .and()
                 .httpBasic()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
     }
 }
